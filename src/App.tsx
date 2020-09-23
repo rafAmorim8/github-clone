47ea764e6d818d2bc085cb2aa0010a0d96dcc228
rafAmorim8 <rafael_amorim30@hotmail.com>
2020-09-23 15:57:08 -0700

    Creates application routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import GlobalStyles from './styles/GlobalStyles';
+
+import Header from './components/Header';
+import Profile from './pages/Profile';
+import Repo from './pages/Repo';
 
 function App() {
   return (
-    <h1>Rocketseat</h1>
+    <BrowserRouter>
+
+      <GlobalStyles />
+
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Profile />} />
+        <Route path="/:username" element={<Profile />} />
+        <Route path="/:username/:repo" element={<Repo />} />
+      </Routes>
+
+      {/* <Footer /> */}
+    </BrowserRouter>
   );
 }
 
