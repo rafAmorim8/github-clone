eca0a62720dadddad5253f5cd84d3d9dfda0dd0a
rafAmorim8 <rafael_amorim30@hotmail.com>
2020-09-23 15:58:27 -0700

    Creates starting pages

---
+++ src/pages/Repo/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Repo: React.FC = () => {
+  return (
+    <Container>
+      <h1>Repo</h1>
+    </Container>
+  );
+};
+
+export default Repo;
